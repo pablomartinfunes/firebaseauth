a380b0b4afd6005634273873b525c9b67b4c9bdd
pablo <pabfun7@gmail.com>
2018-09-28 18:54:35 -0300

    Firebase Auth with Google

--- app/src/main/java/com/gennielabs/firebase/firebaseauth/AccessAccount.java
+++ app/src/main/java/com/gennielabs/firebase/firebaseauth/AccessAccount.java
@@ -1,13 +1,30 @@
 package com.gennielabs.firebase.firebaseauth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AccessAccount extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private Button btnSignOut;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +33,64 @@ public class AccessAccount extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        String name = getIntent().getStringExtra("username");
+        String email = getIntent().getStringExtra("email");
+
+        TextView uName = (TextView) findViewById(R.id.userNameTV);
+        TextView uEmail = (TextView) findViewById(R.id.emailTV);
+
+        uName.setText(name);
+        uEmail.setText(email);
+
+        mAuth = FirebaseAuth.getInstance();
+        btnSignOut = (Button) findViewById(R.id.btLogout);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                                    @Override
+                                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                                    }
+                                })
+                                .addApi(Auth.GOOGLE_SIGN_IN_API)
+                                .build();
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mAuth.signOut();
+
+                // Google sign out
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+
+                    }
+                });
+
+                startActivity(new Intent(AccessAccount.this, MainActivity.class));
+            }
+        });
+
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() == null)
+                    startActivity(new Intent(AccessAccount.this, MainActivity.class));
+
+            }
+        };
+
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(firebaseAuthListener);
+    }
 }
